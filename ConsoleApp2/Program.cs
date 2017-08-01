eec423b12ab32385f1447f7309f15d70146400b8
Antonio <antonio958@hotmail.com>
2017-08-01 13:38:03 +0200

    Eventos

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,12 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp2
+namespace Eventos
 {
     class Program
     {
         static void Main(string[] args)
         {
         }
+    }
+
+    class Ticket
+    {
+
+    }
+
+    class Caja
+    {
+
+        public Caja() {
+            this.tickets = new List<Ticket>();
+        }
+
+        public int Id { get; set; }
+        public List<Ticket> tickets { get; private set; }
+        
+    }
+
+    public delegate void TotalEventHandler(object o, TotalEventArgs e);
+
+    public class TotalEventArgs:EventArgs
+    {
+        public TotalEventArgs(int id, decimal total) {
+
+            Id = id;
+            Total = total;
+        }
+
+        public int Id { get; private set; }
+        public decimal Total { get; private set; }
     }
 }
